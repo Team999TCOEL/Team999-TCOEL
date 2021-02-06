d1040b2a5c7e8a2cd43f09762d35530bc0f73155
Peedeedle <s1806443@connect.glos.ac.uk>
2021-02-06 10:48:26 +0000

    Fixed heart system and added Cracked hearts

--- CT6008-TCOEL/Assets/Scripts/UI/UI_Health.cs
+++ CT6008-TCOEL/Assets/Scripts/UI/UI_Health.cs
@@ -4,71 +4,50 @@
 // Date Created:         <04/02/2021>
 // Brief:                <File responsible for controlling the players health through visualization of the UI>
 // Last Edited By:       <Jack Peedle>
-// Last Edited Date:     <04/02/2021>
-// Last Edit Brief:      <Setting up Health to visualize current health>
+// Last Edited Date:     <06/02/2021>
+// Last Edit Brief:      <Creating an easier way to take hearts away for future proofing>
 ////////////////////////////////////////////////////////////
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Health : MonoBehaviour
 {
-    
-    public GameObject goHeart1;
-    public GameObject goHeart2;
-    public GameObject goHeart3;
+    // Integer values for the health and number of hearts
+    public int iHealth;
+    public int iNumOfHearts;
 
+    public Image[] hearts;
+    public Sprite sFullHeart;
+    public Sprite sBrokenHeart;
 
-    public GameObject goBrokenHeart1;
-    public GameObject goBrokenHeart2;
-    public GameObject goBrokenHeart3;
+    // Update
+    void Update() {
 
-    
-    // Update is called once per frame
-    void Update()
-    {
-        // If the spacebar is pressed change the health
-        if (Input.GetKeyDown("space")) {
-            Debug.Log("SpacePressed");
-            DestroyHearts();
+        // If health is more than the number of hearts then set health to number of hearts
+        if (iHealth > iNumOfHearts) {
+            iHealth = iNumOfHearts;
         }
+
+        // If i is less than hearts in heart array
+        for (int i = 0; i < hearts.Length; i++) {
+
+            // if i is less than health, set heart to full heart sprite
+            if (i < iHealth) {
+                hearts[i].sprite = sFullHeart;
+            } else { // if i is more than health, set heart to broken heart sprite
+                hearts[i].sprite = sBrokenHeart;
+            }
+
+            // Check if i is smaller, if true set hearts to true
+            if (i < iNumOfHearts) {
+                hearts[i].enabled = true;
+            } else { // Check if i is bigger, hide hearts
+                hearts[i].enabled = false;
+            }
+        }     
     }
-    public void DestroyHearts() {
-        // If all hearts are active and player takes damage, show the appropriate broken heart
-        if (goHeart1 && goHeart2 && goHeart3) {
-            goHeart3.SetActive(false);
-            goBrokenHeart3.SetActive(true);
-            Debug.Log("FirstHalfGone");
-            return;
-        }
-        // if the player has 2 and a half hearts, set the half heart to false
-        if (goHeart1 && goHeart2 && goBrokenHeart3 && !goHeart3) {
-            goBrokenHeart3.SetActive(false);
-            Debug.Log("SecondHalfGone");
-            return;
-        }
-        // if the player has 2 hearts make the right one break in half
-        if (goHeart1 && goHeart2) {
-            goHeart2.SetActive(false);
-            goBrokenHeart2.SetActive(true);
-            return;
-        }
-        //if the player has a heart and a half, set the half heart to false
-        if (goHeart1 && goBrokenHeart2) {
-            goBrokenHeart2.SetActive(false);
-            return;
-        }
-        // if the player has one heart, show a half heart
-        if (goHeart1) {
-            goHeart1.SetActive(false);
-            goBrokenHeart1.SetActive(true);
-            return;
-        }
-        //if the player has half a heart and takes damage, game over
-        if (goBrokenHeart1) {
-            Debug.Log("GameOver");
-            return;
-        }
-    }
+
 }
